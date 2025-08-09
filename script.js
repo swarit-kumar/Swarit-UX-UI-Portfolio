bc77fe5ba15cc6ed2bc4f139730967905c45f582
Swarit Kumar <swarit.kumar2005@gmail.com>
2025-08-09 20:19:16 +0530

    Update final script.js

--- script.js
+++ script.js
@@ -24,6 +24,11 @@ window.addEventListener("load", () => {
   }, 16800); // 10 seconds delay after the second message
 });
 
+const aiAgent = document.getElementById("ai-agent");
+
+aiAgent.addEventListener("click", () => {
+  say("Scroll down slowly ⬇️", 4000); // Show the message for 4 seconds
+});
 
 
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
@@ -170,4 +175,218 @@ gsap.from(".grid", {
 
 
 
+
+
+
+(function() {
+  "use strict";
+  
+  var card = document.getElementById('card'),
+      gloss = card.querySelector('.card__gloss'),
+      content = card.querySelector('.card__content'),
+      width = window.innerWidth,
+      height = window.innerHeight;
+  
+  
+  function init() {
+    bindMouse();
+    
+    // Initial tilt
+    light(350, -125);
+    tilt(350, -125);    
+  }
+  
+  function bindMouse() {
+    document.addEventListener('mousemove', (event) => {
+      let x = event.clientX - (width / 2),
+          y = event.clientY - (height / 2);
+
+      light(x, y);
+      tilt(x, y);
+    });
+  }
+  
+  
+  function light(x, y) {
+    let angle = (Math.atan2(y, x) * 180) / Math.PI - 90;
+    
+    gloss.style.background = 'linear-gradient(' + angle + 'deg, rgba(255, 255, 255,' + y / height * .9 + ') 0%, rgba(255, 255, 255, 0) 80%)';    
+  }
+  
+  function tilt(x, y) {
+    let force = 80,
+        rx = (x / width) * force,
+        ry = (y / height) * -force;
+
+    card.style.transform = 'rotateY(' + (rx) + 'deg) rotateX(' + (ry) + 'deg)';    
+    // content.style.transform = 'translateX(' + (rx * .75) + 'px) translateY(' + (ry * .75) + 'px)';
+  }
+  
+  
+  
+  
+  init();
+  
+})();
+
+
+
+
+
+(function() {
+  "use strict";
+  
+  var card = document.getElementById('cardq'),
+      gloss = card.querySelector('.cardq__gloss'),
+      content = card.querySelector('.cardq__content'),
+      width = window.innerWidth,
+      height = window.innerHeight;
+  
+  
+  function init() {
+    bindMouse();
+    
+    // Initial tilt
+    light(350, -125);
+    tilt(350, -125);    
+  }
+  
+  function bindMouse() {
+    document.addEventListener('mousemove', (event) => {
+      let x = event.clientX - (width / 2),
+          y = event.clientY - (height / 2);
+
+      light(x, y);
+      tilt(x, y);
+    });
+  }
+  
+  
+  function light(x, y) {
+    let angle = (Math.atan2(y, x) * 180) / Math.PI - 90;
+    
+    gloss.style.background = 'linear-gradient(' + angle + 'deg, rgba(255, 255, 255,' + y / height * .9 + ') 0%, rgba(255, 255, 255, 0) 80%)';    
+  }
+  
+  function tilt(x, y) {
+    let force = 80,
+        rx = (x / width) * force,
+        ry = (y / height) * -force;
+
+    card.style.transform = 'rotateY(' + (rx) + 'deg) rotateX(' + (ry) + 'deg)';    
+    // content.style.transform = 'translateX(' + (rx * .75) + 'px) translateY(' + (ry * .75) + 'px)';
+  }
+  
+  
+  
+  
+  init();
+  
+})();
+
+
+
+(function() {
+  "use strict";
+  
+  var card = document.getElementById('cardw'),
+      gloss = card.querySelector('.cardw__gloss'),
+      content = card.querySelector('.cardw__content'),
+      width = window.innerWidth,
+      height = window.innerHeight;
+  
+  
+  function init() {
+    bindMouse();
+    
+    // Initial tilt
+    light(350, -125);
+    tilt(350, -125);    
+  }
+  
+  function bindMouse() {
+    document.addEventListener('mousemove', (event) => {
+      let x = event.clientX - (width / 2),
+          y = event.clientY - (height / 2);
+
+      light(x, y);
+      tilt(x, y);
+    });
+  }
+  
+  
+  function light(x, y) {
+    let angle = (Math.atan2(y, x) * 180) / Math.PI - 90;
+    
+    gloss.style.background = 'linear-gradient(' + angle + 'deg, rgba(255, 255, 255,' + y / height * .9 + ') 0%, rgba(255, 255, 255, 0) 80%)';    
+  }
+  
+  function tilt(x, y) {
+    let force = 80,
+        rx = (x / width) * force,
+        ry = (y / height) * -force;
+
+    card.style.transform = 'rotateY(' + (rx) + 'deg) rotateX(' + (ry) + 'deg)';    
+    // content.style.transform = 'translateX(' + (rx * .75) + 'px) translateY(' + (ry * .75) + 'px)';
+  }
+  
+  
+  
+  
+  init();
+  
+})();
+
+
+
+(function() {
+  "use strict";
+  
+  var card = document.getElementById('carde'),
+      gloss = card.querySelector('.carde__gloss'),
+      content = card.querySelector('.carde__content'),
+      width = window.innerWidth,
+      height = window.innerHeight;
+  
+  
+  function init() {
+    bindMouse();
+    
+    // Initial tilt
+    light(350, -125);
+    tilt(350, -125);    
+  }
+  
+  function bindMouse() {
+    document.addEventListener('mousemove', (event) => {
+      let x = event.clientX - (width / 2),
+          y = event.clientY - (height / 2);
+
+      light(x, y);
+      tilt(x, y);
+    });
+  }
+  
+  
+  function light(x, y) {
+    let angle = (Math.atan2(y, x) * 180) / Math.PI - 90;
+    
+    gloss.style.background = 'linear-gradient(' + angle + 'deg, rgba(255, 255, 255,' + y / height * .9 + ') 0%, rgba(255, 255, 255, 0) 80%)';    
+  }
+  
+  function tilt(x, y) {
+    let force = 80,
+        rx = (x / width) * force,
+        ry = (y / height) * -force;
+
+    card.style.transform = 'rotateY(' + (rx) + 'deg) rotateX(' + (ry) + 'deg)';    
+    // content.style.transform = 'translateX(' + (rx * .75) + 'px) translateY(' + (ry * .75) + 'px)';
+  }
+  
+  
+  
+  
+  init();
+  
+})();
 
